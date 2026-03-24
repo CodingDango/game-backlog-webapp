d34293be485ee39312b1250f24b84c12bca76509
CodingDango <babyyodaman2.0@gmail.com>
2026-03-25 02:51:25 +0800

    Changed favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: "Game Backlog",
-  description: "A Library for tracking your games",
+  description: "A Library for tracking your games.",
 };
 
 export default function RootLayout({
